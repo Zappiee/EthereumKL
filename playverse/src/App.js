85418f0b6bc7e896b2680b30981cb234e84ffe71
Kinglee0907 <leekahguen@gmail.com>
2023-10-15 01:58:55 +0800

    ui made

--- playverse/src/App.js
+++ playverse/src/App.js
@@ -76,4 +76,18 @@ function App() {
   );
 }
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
 export default App;
